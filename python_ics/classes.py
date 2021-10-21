dcef628a275c06c18e0ab04fa744d0637bbafc52
luaroncrew <ma-kir.ru@yandex.ru>
2021-10-21 14:30:08 +0200

    data validation methods added to event

--- python_ics/classes.py
+++ python_ics/classes.py
@@ -1,3 +1,4 @@
+import datetime
 from dataclasses import dataclass
 import datetime as dt
 
@@ -8,14 +9,14 @@ class Event:
             title: str,
             dt_start: dt.datetime,
             dt_end: dt.datetime,
-            location: str or None,
-            notes):
+            location=None,
+            description=None):
 
-        self.title = title
+        self.title = self._is_valid_title(title)
         self.location = self._is_valid_location(location)
-        self.dt_start = dt_start
-        self.dt_end = dt_end
-        self.notes = notes
+        self.dt_start = self._is_valid_dt(dt_start)
+        self.dt_end = self._is_valid_dt(dt_end)
+        self.notes = self._is_valid_description(description)
 
     def _is_valid_location(self, location):
         if location is not None:
@@ -23,14 +24,37 @@ class Event:
                 raise TypeError('location can be str or None type')
         return location
 
+    def _is_valid_dt(self, dt_param):
+        if not isinstance(dt_param, dt.datetime):
+            raise TypeError('dt_start/end attributes can be datetime.datetime type only')
+        return dt_param
+
+    def _is_valid_title(self, title):
+        if not isinstance(title, str):
+            raise TypeError('title can be str type only')
+        if len(title) > 75:
+            raise ValueError('title cannot be longer than 75 symbols')
+        return title
+
+    def _is_valid_description(self, notes):
+        if notes is not None:
+            if not isinstance(notes, str):
+                raise TypeError('notes can be str or None type')
+        if len(notes) > 75:
+            raise ValueError('title cannot be longer than 75 symbols')
+        return notes
+
+    def __str__(self):
+        return f'{self.title}, {self.location}, {self.notes}, {self.dt_end}, {self.dt_start}'
+
 
 @dataclass
 class CalendarSetup:
     timezone: str
 
-    def get_setup(self):
-        setup = str()
-        return setup
+    def string(self):
+        return str()
+
 
 
 @dataclass
@@ -40,5 +64,27 @@ class Calendar:
 
     def get_execution_string(self):
         execution_string = str()
-        execution_string += self.setup.get_setup()
+        execution_string += self.setup.string()
 
+        for event in self.events:
+            # making ics compatible strings out of datetime objects
+            st_date = event.dt_start.strftime('%Y%m%d')
+            st_time = 'T' + event.dt_start.strftime('%H%M%S')
+            st_datetime = st_date + st_time
+
+            end_date = event.dt_end.strftime('%Y%m%d')
+            end_time = 'T' + event.dt_end.strftime('%H%M%S')
+            end_datetime = end_date + end_time
+
+            # writing event block
+            event_string = 'BEGIN:VEVENT\n'
+            event_string += 'DTSTAMP:20210904T194914Z\n'  # here we have to add a custom date
+            event_string += f'DTSTART;TZID=Europe/Paris:{st_datetime}\n'
+            event_string += f'DTEND;TZID=Europe/Paris:{end_datetime}\n'
+            event_string += f'SUMMARY:{event.title}\n'
+            if event.location is not None:
+                event_string += f'LOCATION:{event.location}\n'
+
+            if event.description is not None:
+                event_string += f'DESCRIPTION:{event.description}'
+            event_string += 'END:VEVENT\n'
